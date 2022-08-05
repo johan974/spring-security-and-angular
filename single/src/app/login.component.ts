a45554290fca99067703c4ca5e4879783aa18f48
johan974 <jj974@upcmail.nl>
2022-08-05 11:38:49 +0200

    Added: spring session + redis

--- single/src/app/login.component.ts
+++ single/src/app/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AppService } from './app.service';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
